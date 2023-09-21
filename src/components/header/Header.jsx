0bcfae7f95928365202d4fcd7bbd106f431aadeb
AmanWorku <amexworku2014@gmail.com>
2023-09-21 15:49:12 +0300

    Add close button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -44,6 +44,11 @@ const Header = () => {
                         </a>
                     </li>
                 </ul>
+
+                <i className='uil uil-times nav__close'></i>
+            </div>
+            <div className="nav__toggle">
+                <i className='uil uil-apps'></i>
             </div>
         </nav>
     </header>
