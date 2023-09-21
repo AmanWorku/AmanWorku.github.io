815674fe145c3ec14fd6b46b9f3f7dd154f17f87
AmanWorku <amexworku2014@gmail.com>
2023-09-21 20:48:27 +0300

    Add social icons

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Social = () => {
   return (
-    <div>Social</div>
+    <div className="home__social"> 
+        <a href="https://instagram.com/" className="home__social-ico n" target='_blank'>
+            <i className='uil uil-instagram'></i>
+        </a>
+        <a href="https://github.com/" className="home__social-icon" target='_blank'>
+        <i className='uil uil-github-alt'></i>
+        </a>
+        <a href="https://linkedin.com/" className="home__social-icon" target='_blank'>
+        <i className='uil uil-linkedin'></i>
+        </a>
+    </div>
   )
 }
 
