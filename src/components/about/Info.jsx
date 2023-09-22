5feee888130a827b2033d64761182d16dd4f272a
AmanWorku <amexworku2014@gmail.com>
2023-09-22 09:47:07 +0300

    Add info section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+        <div className="about__box">
+            <h3 className="about__title">Experience</h3>
+            <span className="about__subtitle">3 Months Working</span>
+        </div>
+        <div className="about__box">
+            <h3 className="about__title">Completed</h3>
+            <span className="about__subtitle">12+ Projects</span>
+        </div>
+        <div className="about__box">
+            <h3 className="about__title">Support</h3>
+            <span className="about__subtitle">Online 24/6</span>
+        </div>
+    </div>
+  )
+}
+
+export default Info
