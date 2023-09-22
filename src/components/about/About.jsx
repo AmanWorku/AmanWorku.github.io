f66a3fe087c6bb6369077ea92c02d4ed723f6f68
AmanWorku <amexworku2014@gmail.com>
2023-09-22 09:39:32 +0300

    Add about section in the app

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import "./about.css"
+import AboutImg from "../../assets/profile3.png"
+import CV from "../../assets/John-Cv.pdf"
 
 const About = () => {
   return (
 <section className="about section" id="about">
     <h2 className="section__title">About Me</h2>
-    <span className="section__subtitle"></span>
+    <span className="section__subtitle">My Introduction</span>
+    <div className="about__container container grid">
+        <img src={AboutImg} alt="" className="about__img" />
+        <div className="about__data"></div>
+    </div>
 </section>
   )
 }
