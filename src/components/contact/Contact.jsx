e3b2c28063e0c0b548124c6e8d9ebe714e98cb46
AmanWorku <amexworku2014@gmail.com>
2023-09-25 17:40:52 +0300

    Add data to page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,9 +9,26 @@ const Contact = () => {
     <div className="contact__container container grid">
         <div className="contact__content">
             <h3 className="contact__title">Talk to me</h3>
+
+            <div className="contact__info">
+                <div className="contact__card">
+                    <i className="bx bx contact__card-icon"></i>
+                    <h3 className="contact__card-title">Email</h3>
+                    <span className="contact__card-data">amanwtsegaw@gmail.com</span>
+
+                    <a href="" className="contact__button">Write me {" "}<i className="bx bx-right-arrow-alt contact __button-icon"></i></a>
+                </div>
+                <div className="contact__card">
+                    <i className="bx bx contact__card-icon"></i>
+                    <h3 className="contact__card-title">WhatsApp</h3>
+                    <span className="contact__card-data">+2519 3303 9057</span>
+
+                    <a href="" className="contact__button">Write me {" "}<i className="bx bx-right-arrow-alt contact __button-icon"></i></a>
+                </div>
+            </div>
         </div>
         <div className="contact__content">
-            <h3 className="contact__title">Talk to me</h3>
+            <h3 className="contact__title">Write me your project</h3>
         </div>
     </div>
     </section>
