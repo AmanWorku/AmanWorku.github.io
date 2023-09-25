1c3fee530058f2f0b95f4153024f7508985e328d
AmanWorku <amexworku2014@gmail.com>
2023-09-25 17:35:58 +0300

    Create a contact me section

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./contact.css"
+const Contact = () => {
+  return (
+    <section className="contact section" id="contact">
+          <h2 className="section__title">Get In Touch</h2>
+    <span className="section__subtitle">Contact Me</span>
+
+    <div className="contact__container container grid">
+        <div className="contact__content">
+            <h3 className="contact__title">Talk to me</h3>
+        </div>
+        <div className="contact__content">
+            <h3 className="contact__title">Talk to me</h3>
+        </div>
+    </div>
+    </section>
+  )
+}
+
+export default Contact
