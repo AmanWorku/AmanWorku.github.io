840fccce884ed6a7a92ae148249d9d462c4248e3
AmanWorku <amexworku2014@gmail.com>
2023-09-25 17:06:46 +0300

    Add testimonials section

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./testimonials.css"
+const Testimonials = () => {
+  return (
+    <section className="testimonial container section">
+         <h2 className="section__title">Qualification</h2>
+    <span className="section__subtitle">My Personal Journey</span>
+
+    </section>
+  )
+}
+
+export default Testimonials
