bcc7b1c3d523243efee81cf175c2f67805b88036
AmanWorku <amexworku2014@gmail.com>
2023-09-25 16:08:13 +0300

    Add icons and education with experience

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 import "./qualification.css"
 const Qualification = () => {
   return (
-    <div>Qualification</div>
+   <section className="qualification section">
+     <h2 className="section__title">Qualification</h2>
+    <span className="section__subtitle">My Personal Journey</span>
+
+    <div className="qualification__container container">
+        <div className="qualification_tabs">
+            <div className="qualification__button qualification__active button--flex">
+                <i className="uil uil-graduation-cap qualification__icon">
+                </i>{" "} Education
+            </div>
+            <div className="qualification__button qualification__active button--flex">
+                <i className="uil uil-briefcase-alt qualification__icon">
+                </i>{" "} Experience
+            </div>
+        
+        </div>
+    </div>
+   </section>
   )
 }
 
