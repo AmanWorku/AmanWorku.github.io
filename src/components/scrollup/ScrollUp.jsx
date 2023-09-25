0f82bff47da983a9bacbd4eaf63cad18bad10c6a
AmanWorku <amexworku2014@gmail.com>
2023-09-25 19:02:38 +0300

    Finish working with the scroll up button

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -8,7 +8,7 @@ window.addEventListener ("scroll", function(){
     else scrollUp.classList.remove("show-scroll");
 })
   return (
-    <a href="" className="scrollup">
+    <a href="#" className="scrollup">
       <i className="uil uil-arrow-up scrollup__icon"></i>
     </a>
   )
