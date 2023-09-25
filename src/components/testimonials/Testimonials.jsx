0eb0ff51a649ee1a1499444bb18e2e1b0475b272
AmanWorku <amexworku2014@gmail.com>
2023-09-25 17:22:23 +0300

    Add a swiper to testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { Data } from "./Data";
 import "./testimonials.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 const Testimonials = () => {
   return (
     <section className="testimonial container section">
          <h2 className="section__title">Testimonial</h2>
     <span className="section__subtitle">My Partners Say</span>
-    <div className="testimonial__container">
+    <Swiper className="testimonial__container"
+    loop={true}
+    grabCursor={true}
+    spaceBetween={24}
+    pagination={{
+      clickable: true,
+    }}
+    breakpoints={{
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+    }}
+    modules={[Pagination]}>
         {Data.map(({id, image, title, description}) => {
             return(
-                <div className="testimonial__card" key={id}>
+                <SwiperSlide className="testimonial__card" key={id}>
                     <img src={image} alt='' className="testimonial__img"/>
                     <h3 className="testimonial__name">{title}</h3>
                     <p className="testimonial__description">{description}</p>
-                </div>
+                </SwiperSlide>
             )
         })}
-    </div>
+    </Swiper>
     </section>
   )
 }
