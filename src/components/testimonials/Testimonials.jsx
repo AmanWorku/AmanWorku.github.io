00eba5746cdf118819d4d86d3f5151ac86e92e41
AmanWorku <amexworku2014@gmail.com>
2023-09-25 17:12:55 +0300

    Add sample testimonial data

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Data } from "./Data";
 import "./testimonials.css"
 const Testimonials = () => {
   return (
     <section className="testimonial container section">
-         <h2 className="section__title">Qualification</h2>
-    <span className="section__subtitle">My Personal Journey</span>
-
+         <h2 className="section__title">Testimonial</h2>
+    <span className="section__subtitle">My Partners Say</span>
+    <div className="testimonial__container"></div>
     </section>
   )
 }
