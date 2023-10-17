a589c802aee061622a3b3e4f8fcbce63ebd3d2de
AmanWorku <amexworku2014@gmail.com>
2023-10-17 07:05:59 +0300

    Add services in a different file

---
+++ src/components/services/serviceData.jsx
@@ -0,0 +1,20 @@
+const serviceData = [
+    {
+      id: 1,
+      title: "Product Designer",
+      description: "Service with more than 3 years of experience...",
+    },
+    {
+      id: 2,
+      title: "UI/UX Designer",
+      description: "Service with more than 3 years of experience...",
+    },
+    {
+      id: 3,
+      title: "UI/UX Designer",
+      description: "Service with more than 3 years of experience...",
+    },
+  ];
+  
+  export default serviceData;
+  
