e53f9821b25801b199b426a823b858b6f45a8a64
AmanWorku <amexworku2014@gmail.com>
2023-10-17 07:24:53 +0300

    Update projects section

--- src/components/services/serviceData.jsx
+++ src/components/services/serviceData.jsx
@@ -1,18 +1,24 @@
 const serviceData = [
     {
       id: 1,
-      title: "Product Designer",
-      description: "Service with more than 3 years of experience...",
+      title: "ConcertHub",
+      description: "ConcertHub is a platform where music enthusiasts can discover upcoming concerts, book tickets, and explore their favorite artists. Experience the world of music like never before with ConcertHub! This is a late post of our final capstone project in the Microverse technical curriculum.",
+      live_demo: "https://concert-react-front-end.vercel.app/",
+      source_link: "https://concert-react-front-end.vercel.app/",
     },
     {
       id: 2,
       title: "UI/UX Designer",
       description: "Service with more than 3 years of experience...",
+      live_demo: "https://concert-react-front-end.vercel.app/",
+      source_link: "https://concert-react-front-end.vercel.app/",
     },
     {
       id: 3,
       title: "UI/UX Designer",
       description: "Service with more than 3 years of experience...",
+      live_demo: "https://concert-react-front-end.vercel.app/",
+      source_link: "https://concert-react-front-end.vercel.app/",
     },
   ];
   
