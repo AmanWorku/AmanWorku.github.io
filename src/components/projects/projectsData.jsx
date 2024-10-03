4f132793759c9918b41a1771e560769e7125e25b
AmanWorku <amexworku2014@gmail.com>
2024-10-03 13:23:49 +0300

    Update content

--- src/components/projects/projectsData.jsx
+++ src/components/projects/projectsData.jsx
@@ -5,73 +5,81 @@ import country from "./../../assets/projects/country.png";
 import budget from "./../../assets/projects/budget.png";
 import PixelBros from "./../../assets/projects/pixelbros.png";
 import space from "./../../assets/projects/space.png";
-import landing from "./../../assets/projects/landing.webp";
+import ezraseminary from "./../../assets/projects/ezraseminary.png";
 
 const projectData = [
-    {
-      id: 1,
-      title: "ConcertHub",
-      description: "ConcertHub is a platform where music enthusiasts can discover upcoming concerts, book tickets, and explore their favorite artists. Experience the world of music like never before with ConcertHub! This is a late post of our final capstone project in the Microverse technical curriculum.",
-      live_demo: "https://concert-react-front-end.vercel.app/",
-      source_link: "https://github.com/AmanWorku/ConcertHub-Frontend",
-      image: concertImage,
-    },
-    {
-      id: 2,
-      title: "Budget Mate",
-      description: "Budget Mate is a simple web application that helps users keep track of their personal finances and budgeting goals. The application allows users to create a budget and track their spending across different categories such as groceries, transportation, and entertainment.",
-      live_demo: "https://concert-react-front-end.vercel.app/",
-      source_link: "https://budget-mate-z4ax.onrender.com//",
-      image: budget,
-    },
-    {
-      id: 3,
-      title: "World Gallery",
-      description: "World Gallery is a SPA which provides users to look into the list of countries that are in the world. They can also click on their preferred country and see the details about a country.",
-      live_demo: "https://world-gallery.onrender.com/",
-      source_link: "https://github.com/AmanWorku/World-Gallery",
-      image: country,
-    },
-      {id: 4,
-      title: "Book Store",
-      description: "A simple bookstore app built with React and React that enables a user to add a book with an author name.",
-      live_demo: "https://bookstore-by-aman.onrender.com/",
-      source_link: "https://github.com/AmanWorku/Bookstore",
-      image: bookImage,
-    },
-    {
-      id: 5,
-      title: "Math Magician",
-      description: "A simple calculator app built with React. It additionally has a quote at the quote section.",
-      live_demo: "https://calculator-by-aman.onrender.com/",
-      source_link: "https://github.com/AmanWorku/Math-magician",
-      image: calcImage,
-    },
-    {
-      id: 6,
-      title: "PixelBros",
-      description: "PixelBros is a capstone project made using HTML, CSS and JavaScript at the end of Module 2 during our study in Microverse. In this website, users can view all the retro game characters,and add likes/comments on their favourite ones.",
-      live_demo: "https://indiecodermm.github.io/js-capstone/",
-      source_link: "https://github.com/AmanWorku/JS-Capstone",
-      image: PixelBros,
-    },
-    {
-        id: 7,
-        title: "Space Travelers' Hub",
-        description: "Space Travelers' Hub is a SPA which provides users to reserve specific rockets and dragons with a mission which is fetched from external APIs. It also displays the selected projects in My Profile page.",
-        live_demo: "https://space-travlers-hub.onrender.com/",
-        source_link: "https://github.com/AmanWorku/Space-Travelers-Hub",
-        image: space,
-      },
-    {
-        id: 8,
-        title: "Landing Page",
-        description: "A musical teaching website created as a capstone project while finishing the first module in microverse. Built with HTML, CSS and JavaScript.",
-        live_demo: "https://amanworku.github.io/Module-1-Capstone-Project/",
-        source_link: "https://github.com/AmanWorku/Module-1-Capstone-Project",
-        image: landing,
-      },
-  ];
-  
-  export default projectData;
-  
+	{
+		id: 1,
+		title: "Ezra Seminary",
+		description:
+			"Ezra Seminary is an online bible learning platform that enables user to learn courses, study daily devotionals and quarterly lessons. It has an admin dashboard that enables the course creator or instructor create courses. It gives the users seamless access to the contents.",
+		live_demo: "https://ezraseminary.org",
+		source_link: "https://github.com/EzraSeminary/Ezera_Seminary__Frontend",
+		image: ezraseminary,
+	},
+	{
+		id: 2,
+		title: "ConcertHub",
+		description:
+			"ConcertHub is a platform where music enthusiasts can discover upcoming concerts, book tickets, and explore their favorite artists. Experience the world of music like never before with ConcertHub! This is a late post of our final capstone project in the Microverse technical curriculum.",
+		live_demo: "https://concert-react-front-end.vercel.app/",
+		source_link: "https://github.com/AmanWorku/ConcertHub-Frontend",
+		image: concertImage,
+	},
+	{
+		id: 3,
+		title: "Budget Mate",
+		description:
+			"Budget Mate is a simple web application that helps users keep track of their personal finances and budgeting goals. The application allows users to create a budget and track their spending across different categories such as groceries, transportation, and entertainment.",
+		live_demo: "https://concert-react-front-end.vercel.app/",
+		source_link: "https://budget-mate-z4ax.onrender.com//",
+		image: budget,
+	},
+	{
+		id: 4,
+		title: "World Gallery",
+		description:
+			"World Gallery is a SPA which provides users to look into the list of countries that are in the world. They can also click on their preferred country and see the details about a country.",
+		live_demo: "https://world-gallery.onrender.com/",
+		source_link: "https://github.com/AmanWorku/World-Gallery",
+		image: country,
+	},
+	{
+		id: 5,
+		title: "Book Store",
+		description:
+			"A simple bookstore app built with React and React that enables a user to add a book with an author name.",
+		live_demo: "https://bookstore-by-aman.onrender.com/",
+		source_link: "https://github.com/AmanWorku/Bookstore",
+		image: bookImage,
+	},
+	{
+		id: 6,
+		title: "Math Magician",
+		description:
+			"A simple calculator app built with React. It additionally has a quote at the quote section.",
+		live_demo: "https://calculator-by-aman.onrender.com/",
+		source_link: "https://github.com/AmanWorku/Math-magician",
+		image: calcImage,
+	},
+	{
+		id: 7,
+		title: "PixelBros",
+		description:
+			"PixelBros is a capstone project made using HTML, CSS and JavaScript at the end of Module 2 during our study in Microverse. In this website, users can view all the retro game characters,and add likes/comments on their favourite ones.",
+		live_demo: "https://indiecodermm.github.io/js-capstone/",
+		source_link: "https://github.com/AmanWorku/JS-Capstone",
+		image: PixelBros,
+	},
+	{
+		id: 8,
+		title: "Space Travelers' Hub",
+		description:
+			"Space Travelers' Hub is a SPA which provides users to reserve specific rockets and dragons with a mission which is fetched from external APIs. It also displays the selected projects in My Profile page.",
+		live_demo: "https://space-travlers-hub.onrender.com/",
+		source_link: "https://github.com/AmanWorku/Space-Travelers-Hub",
+		image: space,
+	},
+];
+
+export default projectData;
--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,85 +1,93 @@
-import React from 'react'
-import "./qualification.css"
+import React from "react";
+import "./qualification.css";
 const Qualification = () => {
-  return (
-   <section className="qualification section" id="qualification">
-     <h2 className="section__title">Qualification</h2>
-    <span className="section__subtitle">My Personal Journey</span>
+	return (
+		<section className="qualification section" id="qualification">
+			<h2 className="section__title">Qualification</h2>
+			<span className="section__subtitle">My Personal Journey</span>
 
-    <div className="qualification__container container">
-        <div className="qualification__tabs">
-            <div className="qualification__button qualification__active button--flex">
-                <i className="uil uil-graduation-cap qualification__icon">
-                </i>{" "} Education
-            </div>
-            <div className="qualification__button qualification__active button--flex">
-                <i className="uil uil-briefcase-alt qualification__icon">
-                </i>{" "} Experience
-            </div>
-        </div>
-        <div className="qualification__sections">
-            <div className="qualification__content qualification__content-active">
-                <div className="qualification__data">
-                    <div>
-                        <h3 className="qualification__title">Information Technology</h3>
-                        <span className="qualification__subtitle">Debremarkos University - Ethiopia</span>
-                        <div className="qualification__calendar">
-                            <i className="uil uil-calendar-alt"></i>{" "}2018 - 2022
-                        </div>
-                    </div>
-                    <div>
-                        <span className="qualification__rounder"></span>
-                        <span className="qualification__line"></span>
-                    </div>
-                </div>
-                <div className="qualification__data">
-                    <div></div>
-                    <div>
-                        <span className="qualification__rounder"></span>
-                        <span className="qualification__line"></span>
-                    </div>
-                    <div>
-                        <h3 className="qualification__title">Full Stack Developer</h3>
-                        <span className="qualification__subtitle">Biruh Culture PLC</span>
-                        <div className="qualification__calendar">
-                            <i className="uil uil-calendar-alt"></i>{" "}Jun 2023 - Aug 2023
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="qualification__data">
-                    <div>
-                        <h3 className="qualification__title">Full Stack Web Development</h3>
-                        <span className="qualification__subtitle">Microverse - Remote (SanFransisco)</span>
-                        <div className="qualification__calendar">
-                            <i className="uil uil-calendar-alt"></i>{" "}2022 - 2023
-                        </div>
-                    </div>
-                    <div>
-                        <span className="qualification__rounder"></span>
-                        <span className="qualification__line"></span>
-                    </div>
-                </div>
-                <div className="qualification__data">
-                    <div></div>
-                    <div>
-                        <span className="qualification__rounder"></span>
-                        <span className="qualification__line"></span>
-                    </div>
-                    <div>
-                        <h3 className="qualification__title">Graphics Designer</h3>
-                        <span className="qualification__subtitle">YeHiywet Kal Ministry</span>
-                        <div className="qualification__calendar">
-                            <i className="uil uil-calendar-alt"></i>{" "}2021 - Present
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-   </section>
-  )
-}
+			<div className="qualification__container container">
+				<div className="qualification__tabs">
+					<div className="qualification__button qualification__active button--flex">
+						<i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+						Education
+					</div>
+					<div className="qualification__button qualification__active button--flex">
+						<i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
+						Experience
+					</div>
+				</div>
+				<div className="qualification__sections">
+					<div className="qualification__content qualification__content-active">
+						<div className="qualification__data">
+							<div>
+								<h3 className="qualification__title">Information Technology</h3>
+								<span className="qualification__subtitle">
+									Debremarkos University - Ethiopia
+								</span>
+								<div className="qualification__calendar">
+									<i className="uil uil-calendar-alt"></i> 2018 - 2022
+								</div>
+							</div>
+							<div>
+								<span className="qualification__rounder"></span>
+								<span className="qualification__line"></span>
+							</div>
+						</div>
+						<div className="qualification__data">
+							<div></div>
+							<div>
+								<span className="qualification__rounder"></span>
+								<span className="qualification__line"></span>
+							</div>
+							<div>
+								<h3 className="qualification__title">Full Stack Developer</h3>
+								<span className="qualification__subtitle">
+									Biruh Culture PLC
+								</span>
+								<div className="qualification__calendar">
+									<i className="uil uil-calendar-alt"></i> Jun 2023 - Aug 2023
+								</div>
+							</div>
+						</div>
+						<div className="qualification__data">
+							<div>
+								<h3 className="qualification__title">
+									Full Stack Web Development
+								</h3>
+								<span className="qualification__subtitle">
+									Microverse - Remote (SanFransisco)
+								</span>
+								<div className="qualification__calendar">
+									<i className="uil uil-calendar-alt"></i> 2022 - 2023
+								</div>
+							</div>
+							<div>
+								<span className="qualification__rounder"></span>
+								<span className="qualification__line"></span>
+							</div>
+						</div>
+						<div className="qualification__data">
+							<div></div>
+							<div>
+								<span className="qualification__rounder"></span>
+								<span className="qualification__line"></span>
+							</div>
+							<div>
+								<h3 className="qualification__title">Full Stack Developmer</h3>
+								<span className="qualification__subtitle">
+									YeHiywet Kal Ministry (Remote)
+								</span>
+								<div className="qualification__calendar">
+									<i className="uil uil-calendar-alt"></i> 2023 - Present
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default Qualification
+export default Qualification;
